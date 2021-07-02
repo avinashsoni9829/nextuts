6c38472b53cde9b445f4e3c5bb043d3cad350e12
avinash9829 <avinashsoni9829@gmail.com>
2021-07-02 21:21:16 +0530

    next

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+const About = () => {
+    return ( 
+        <>
+          <Head>
+     <title>About</title>
+     <meta name="keywords" content="ninjas" />
+    </Head>
+    <div>
+
+       <h1>ABout us Page </h1>  
+        <p>lorem ipsum dolor sit amet, consectetur adipiscnklvnsklnvlskdnvkljsdnvlksdbnvkljsdnbvksdnbvlsdnv</p>
+        <p>lorem ipsum dolor sit amet, consectetur adipiscnklvnsklnvlskdnvkljsdnvlksdbnvkljsdnbvksdnbvlsdnv</p>
+
+    </div>
+    </>
+     );
+}
+ 
+export default About;
